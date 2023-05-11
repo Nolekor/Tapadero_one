9e9fbe4d24b8444072f1cdf5f423ebb7582ed261
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-05-11 17:11:11 +0200

    Setup tailwind and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+import Menu from "./pages/Menu";
 
 function App() {
-
-
   return (
     <>
-     
+      <Header />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
